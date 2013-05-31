159e655159c88867c63996ad2dac528b1eadec60
Jeff Blaisdell <blaisdje@yahoo.com>
2013-05-31 09:26:07 -0500

    cleaning.

--- js/main.js
+++ js/main.js
@@ -1,73 +1,20 @@
 require(['jquery', 'backbone', 'component-factory', 'model', 'data'], function($, Backbone, componentFactory, Model, Data) {
 
-	var builder = {};
+	var product        = undefined,
+	    features       = undefined,
+	    selections     = undefined,
+	    optionFeatures = undefined,
+	    feature        = undefined,
+	    selection      = undefined;
 
-	builder.launchRing = function() {
+	var builder        = {}; // Namespace for all controller behaviors.
+    var events         = {}; // Namespace for all event handlers.
+    events.helper      = {}; // Sub-namespace for event helper functions.
 
-		var product    = new Model.Product(Data.ring),
-		    selections = product.configurations,
-		    feature    = product.features.get('METAL CHOICE'),
-		    selection      = selections.get(feature.get('featureId'));
-
-	    selections.on('change:optionOsr', function( selection ) {
-	    	console.log('Your selection has changed to : "' + selection.get('optionDisplayName') + '" from "' + selection.previous('optionDisplayName') + '".');
-	    });
-
-		componentFactory.render( feature, selections )
-	    	.then(function( selection ) {
-	    		var component = selection.component;
-	    		$('body').append(component.get('html'));
-	    		component.trigger('ready', selection);
-	    	});
-
-	};
-
-	builder.launchPendant = function() {
-
-		var product        = new Model.Product(Data.pendant),
-		    features       = product.features,
-		    selections     = product.configurations,
-		    optionFeatures = product.optionFeatures,
-		    feature        = product.features.get('PENDANT ENGRAVING'),
-		    selection      = selections.get(feature.get('featureId'));
-
-		var events = {};
-		events.isRenderNeeded = function( selection ) {
-            var feature        = features.get(selection.get('featureOsr')),
-                option         = feature.options.get(selection.get('optionId')),
-                prevOption     = ( selection.previous('optionId') ? feature.options.get(selection.previous('optionId')) : undefined),
-                isRenderNeeded = false;
-                
-                /**
-                 * Have sub-features changed?
-                 */
-                if ( prevOption.has('optionFeatureRefId') || option.has('optionFeatureRefId') ) {
-                	return true;
-                }
-            
-            return false;
-		};
-
-		events.onchange = function( selection ) {
-			var isRenderNeeded = false;
-            isRenderNeeded = events.isRenderNeeded( selection );
-
-            if ( isRenderNeeded ) {
-            	componentFactory.render( feature, selections, optionFeatures )
-	    	        .then(function( selection ) {
-	    		        var component = selection.component,
-	    		            selector  = selection.component.get('selector');
-	    		        $(selector).replaceWith(component.get('html'));
-	    		        component.trigger('ready', selection);
-	    	});
-            }
-		};
-
-
-	    selections.on('change:optionOsr', function( selection ) {
-	    	events.onchange( selection );
-	    	console.log('Your selection has changed to : "' + selection.get('optionDisplayName') + '" from "' + selection.previous('optionDisplayName') + '".');
-	    });
+    builder.launch = function() {
+        
+        builder.init();
+        events.init();
 
 		componentFactory.render( feature, selections, optionFeatures )
 	    	.then(function( selection ) {
@@ -76,11 +23,63 @@ require(['jquery', 'backbone', 'component-factory', 'model', 'data'], function($
 	    		component.trigger('ready', selection);
 	    	});
 
-	};	
+    };
 
+    builder.init = function() {
+		product        = new Model.Product(Data.pendant),
+		features       = product.features,
+		selections     = product.configurations,
+		optionFeatures = product.optionFeatures,
+		feature        = product.features.get('PENDANT ENGRAVING'),
+		selection      = selections.get(feature.get('featureId'));
+    };
+    
+    builder.renderSeletion = function( selection ) {
+        var feature = features.get(selection.get('featureOsr'));
+
+		componentFactory.render( feature, selections, optionFeatures )
+	    	.then(function( selection ) {
+	    		var component = selection.component,
+	    		    selector  = component.get('selector');
+
+	    		$(selector).replaceWith(component.get('html'));
+	    		component.trigger('ready', selection);
+	    	});        
+    };
+
+    events.helper.isRenderNeeded = function( selection ) {
+        var feature        = features.get(selection.get('featureOsr')),
+            option         = feature.options.get(selection.get('optionId')),
+            prevOption     = ( selection.previous('optionId') ? feature.options.get(selection.previous('optionId')) : undefined),
+            isRenderNeeded = false;
+                
+            /**
+             * Have sub-features changed?
+             */
+            if ( prevOption.has('optionFeatureRefId') || option.has('optionFeatureRefId') ) {
+             	return true;
+            }
+            
+        return false;
+    };
+
+    events.onchange = function( selection ) {
+
+        isRenderNeeded = events.helper.isRenderNeeded( selection );
+
+        if ( isRenderNeeded ) {
+            builder.renderSeletion( selection );
+        }
+    };
+
+    events.init = function() {
+	    selections.off('change:optionOsr', events.onchange)
+	               .on('change:optionOsr', events.onchange);
+
+    };
 
 	$( document ).ready(function() {
-		builder.launchPendant();
+		builder.launch();
 	});
 
 });
